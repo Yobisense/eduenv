e6f35cd9684a43c202cdac32016852daa7f6d566
Tjad I Clark <tjataz@gmail.com>
2017-02-07 11:36:36 +0200

    ruby-neo4j: Add start/end date to course_instance

--- ruby-neo4j/lib/model/course_instance.rb
+++ ruby-neo4j/lib/model/course_instance.rb
@@ -1,5 +1,8 @@
 module Eduenv
   class CourseInstance < AbstractEntity
+    property :start_date
+    property :end_date
+
     has_one :out, :course, type: :INSTANCE_OF
     has_one :out, :instructor, type: :INSTRUCTED_BY
     has_many :in, :students, type: :ENROLLED_IN
